b2410d3d34466b482ecd1a39fdaebde17cfd0842
KeKeZMen <makeskex@yandex.ru>
2024-08-21 18:38:42 +0300

    fix(api): перевод api на async/await

--- src/entities/question/api.ts
+++ src/entities/question/api.ts
@@ -32,7 +32,7 @@ type GetQuestionsType = {
 
 export const getQuestions = createAsyncThunk(
   "getQuestions",
-  (
+  async (
     {
       easyQuestionsCount,
       mediumQuestionsCount,
@@ -40,45 +40,45 @@ export const getQuestions = createAsyncThunk(
     }: GetQuestionsType,
     thunkApi
   ) => {
+    const requests = [
+      sleep(0).then(() =>
+        baseAxios.get<IResponse>(
+          `/api.php?amount=${easyQuestionsCount}&difficulty=easy`
+        )
+      ),
+
+      sleep(5000).then(() =>
+        baseAxios.get<IResponse>(
+          `/api.php?amount=${mediumQuestionsCount}&difficulty=medium`
+        )
+      ),
+
+      sleep(10000).then(() =>
+        baseAxios.get<IResponse>(
+          `/api.php?amount=${hardQuestionsCount}&difficulty=hard`
+        )
+      ),
+    ];
+
     try {
-      const requests = [
-        sleep(0).then(() =>
-          baseAxios.get<IResponse>(
-            `/api.php?amount=${easyQuestionsCount}&difficulty=easy`
-          )
-        ),
-
-        sleep(5000).then(() =>
-          baseAxios.get<IResponse>(
-            `/api.php?amount=${mediumQuestionsCount}&difficulty=medium`
-          )
-        ),
-
-        sleep(10000).then(() =>
-          baseAxios.get<IResponse>(
-            `/api.php?amount=${hardQuestionsCount}&difficulty=hard`
-          )
-        ),
-      ];
-
-      return Promise.all(requests).then((responses) =>
-        responses.flatMap((obj) =>
-          obj.data.results.map(
-            (question) =>
-              ({
-                ...question,
-                question: decodeHtml(question.question),
-                incorrect_answers: question.incorrect_answers.map(decodeHtml),
-                correct_answer: Array.isArray(question.correct_answer)
-                  ? question.correct_answer.map(decodeHtml)
-                  : [decodeHtml(question.correct_answer)],
-                id: String(Math.floor(Math.random() * 1000000)),
-              } as IQuestionWithId)
-          )
+      const responses = await Promise.all(requests);
+      return responses.flatMap((obj) =>
+        obj.data.results.map(
+          (question) =>
+            ({
+              ...question,
+              question: decodeHtml(question.question),
+              incorrect_answers: question.incorrect_answers.map(decodeHtml),
+              correct_answer: Array.isArray(question.correct_answer)
+                ? question.correct_answer.map(decodeHtml)
+                : [decodeHtml(question.correct_answer)],
+              id: String(Math.floor(Math.random() * 1000000)),
+            } as IQuestionWithId)
         )
       );
-    } catch (error) {
-      return thunkApi.rejectWithValue(error as ErrorResponseType);
+    } catch (err) {
+      const { data, status } = err as ErrorResponseType;
+      return thunkApi.rejectWithValue({ data, status });
     }
   }
 );
