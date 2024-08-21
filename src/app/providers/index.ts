b8edcce82171fb31359125a7c7bc898819044b60
KeKeZMen <makeskex@yandex.ru>
2024-08-21 14:26:03 +0300

    added(providers): added withToaster to compose

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,4 +1,5 @@
 import compose from "compose-function";
 import { withStore } from "./withStore";
+import { withToaster } from "./withToaster";
 
-export const withProviders = compose(withStore);
+export const withProviders = compose(withStore, withToaster);
