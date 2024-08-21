eb7ab40d2ce2a85c8f13d02faa83cdd07b86f9a4
KeKeZMen <makeskex@yandex.ru>
2024-08-21 14:31:31 +0300

    added(utils): added utils funcs

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,2 +1,17 @@
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const shufleQuestionsArray = (array: Array<string>) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[j], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+export const decodeHtml = (html: string) => {
+  const txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+};
