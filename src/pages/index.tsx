49891f56bde283c164d51bce9b13d9c37eb0d423
KeKeZMen <makeskex@yandex.ru>
2024-08-21 14:36:14 +0300

    fix(routing): using layout for questions page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Mainpage } from "./mainpage";
+import { Questionpage } from "./question";
+import { Finalpage } from "./final";
+import { Layout } from "@widgets";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Mainpage />,
+  },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/:questionId",
+        element: <Questionpage />,
+      },
+      {
+        path: "/final",
+        element: <Finalpage />,
+      },
+    ],
   },
 ]);
